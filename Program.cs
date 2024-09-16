69fce0edc340000036b81a79bd60cc2089e1d8b9
Yesveer <shivambhairav@gmail.com>
2024-09-16 06:46:33 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Define a route for the root URL
+app.MapGet("/", () => "Hello, World! Welcome to my ASP.NET Core app.");
+
+// Run the application on port 5001
+app.Run("http://localhost:5001");
+
